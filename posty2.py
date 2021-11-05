61527218f1b45a91f253d8f1e4296f5d66f0b0d4
Elise Gonzalez <elise@ischool.berkeley.edu>
2021-11-05 00:57:15 -0500

    add delivery fee, default sort

--- posty2.py
+++ posty2.py
@@ -2,7 +2,12 @@ from selenium import webdriver
 import os
 import time
 import pandas as pd
+import re
 
+'''11/5 elise changed: f-string formatting in lines 47-49, sort results page 
+by most popular instead of delivery time, add delivery fee column to dataframe,
+update xpaths to relevant elements
+'''
 def check_address(address):
 
     # Chrome path
@@ -10,7 +15,7 @@ def check_address(address):
     # Set up chrome driver
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(chrome_path, options=options)
+    driver = webdriver.Chrome(executable_path=chrome_path, options=options)
     driver.get("https://postmates.com/")
     # Get element with id=location-typeahead-home-input
     element = driver.find_element_by_id("location-typeahead-home-input")
@@ -24,7 +29,9 @@ def check_address(address):
     # Wait for page to load
     time.sleep(10)
     # Get span with text="Delivery Time"
-    span = driver.find_element_by_xpath("//span[text()='  Delivery time']")
+    # ORIGINAL: 
+    # span = driver.find_element_by_xpath("//span[text()='  Delivery time']")
+    span = driver.find_element_by_xpath("//span[text()='  Most popular']")
     span.click()
     time.sleep(10)
     # Get list of results
@@ -36,20 +43,33 @@ def check_address(address):
     # Parse links
     links = [link.get_attribute('href') for link in links_raw]
     # Get list of wait times
-    wait_times = driver.find_elements_by_xpath("//div[@class='ba bv e1 bw bx fe']")
+    # ORIGINAL: 
+    # wait_times = driver.find_elements_by_xpath("//div[@class='ba bv e1 bw bx fe']")
+    # Worked on Delivery Time filter page
+    # wait_times = driver.find_elements_by_xpath("//div[@class='ba bv e1 bw bx ff']")
+    wait_times = driver.find_elements_by_xpath("//div[@class='ck ci ho']")
     # Parse wait times
     wait_times = [wait_time.text for wait_time in wait_times]
+
     # Exclude empty strings
     wait_times = [wait_time for wait_time in wait_times if wait_time != '']
 
+    delivery_fees = driver.find_elements_by_xpath("//div[@class='ck ci br']")
+    delivery_fees = [fee.text for fee in delivery_fees]
+    # delivery_fees = [fee for fee in delivery_fees if fee != ''] # verify that regex doesn't exclude any restaurant entries
+    delivery_fees = [re.search(r"[0-9]*[0-9].[0-9][0-9]", fee).group(0) for fee in delivery_fees if fee != '' and re.search(r"[0-9]*[0-9].[0-9][0-9]", fee)]
     # Print length of lists
-    print(f"Names:{names[0:15]}")
-    print(f"Wait Times:{wait_times[0:15]}")
-    print(f"Links:{links[0:15]}")
+    print('Names:{}'.format(names[0:15]))
+    print('Wait Times:{}'.format(wait_times[0:15]))
+    print('Delivery Fees:{}'.format(delivery_fees[0:15]))
+    print('Links:{}'.format(links[0:15]))
+    print(len(names), len(wait_times), len(delivery_fees), len(links))
+
     # print(ratings[0:15])
 
     # Combine  names, wait times, and links into a dataframe
-    df = pd.DataFrame({'Name': names, 'Wait Time': wait_times, 'Link': links})
+    df = pd.DataFrame({'Name': names, 'Wait Time': wait_times, 'Delivery Fee': delivery_fees, 'Link': links})
+
     # Close driver
     driver.close()
     # Return dataframe
